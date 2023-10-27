80352068f9911bd7dde839f6ac5d36db3e8d7430
Przemysław Bulanda <devprzemas@gmail.com>
2023-10-27 14:50:05 +0200

    Add optionImage JS

--- src/js/script.js
+++ src/js/script.js
@@ -88,6 +88,7 @@ const select = {
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
     initAccordion(){
       const thisProduct = this;
@@ -150,10 +151,9 @@ const select = {
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-
-
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)){
+          if(optionSelected){
             // -----check if the option is not default
             if(!option.default) {
               // ----add option price to variable
@@ -164,7 +164,18 @@ const select = {
             if(option.default) {
               // ----reduce price variable
               price = price - option.price;
-
+            }
+          }
+          const optionImage = thisProduct.imageWrapper.querySelector(`.${paramId}-${optionId}`)
+          const imageActive = classNames.menuProduct.imageVisible;
+          if(optionImage){
+            if(optionSelected){
+              optionImage.classList.add(imageActive);
+              console.log(optionImage, optionSelected);
+            } else {
+              if(!optionSelected){
+                optionImage.classList.remove(imageActive);
+              }
             }
           }
         }
@@ -172,7 +183,6 @@ const select = {
     
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
-
       
     }
   }
